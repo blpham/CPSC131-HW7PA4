5c76df11a0fe7b243e6d13d09ab88b8582d9dcd0
Brenda Pham <blpham21@gmail.com>
2022-12-16 23:15:21 -0800

    update 2

--- max_area_of_island.cpp
+++ max_area_of_island.cpp
@@ -5,77 +5,35 @@ using namespace std;
 
 class Solution {
 public:
-    void dfs(int x, int y, vector<vector<int>>& v, vector<vector<int>>& grid, int& cnt) {
-		cnt++;
-		v[x][y] = 1;
-		if (x > 0 && grid[x - 1][y] == 1 && !v[x - 1][y]) {
-			dfs(x - 1, y, v, grid, cnt);
-		}
-		if (y > 0 && grid[x][y - 1]==1 && !v[x][y - 1]) {
-			dfs(x, y - 1, v, grid, cnt);
-		}
-		if (x < grid.size() - 1 && grid[x + 1][y]==1 && !v[x + 1][y]) {
-			dfs(x + 1, y, v, grid, cnt);
-		}
-		if (y < grid[0].size() - 1 && grid[x][y + 1] == 1 && !v[x][y + 1]) {
-		    dfs(x, y + 1, v, grid, cnt);
-		}
-	}
-	int maxAreaOfIsland(vector<vector<int>>& grid) {
-		vector<vector<int>>v(grid.size(), vector<int>(grid[0].size(), 0));
-		int maxi = 0;
-		for (int i = 0; i < grid.size(); i++) {
-			for (int j = 0; j < grid[0].size(); j++) {
-				if (!v[i][j] && grid[i][j] == 1) {
-					int cnt = 0;
-					dfs(i, j, v, grid, cnt);
-                    v[i][j] = 1;
-					maxi = max(maxi, cnt);
-				}
-			}
-		}
-		return maxi;
-	}
-};
-
-class Solution
-{
-public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size();
-        int n = grid[0].size();
-        int maxArea = 0;
-        for (int i = 0; i < m; i++) { // traverse through all rows
-            for (int j = 0; j < n; j++) { // traverse through all columns
-                if (1 == grid[i][j]) { // if grid value at this index is 1 (start of an island) start a BFS to find the size
-                    maxArea = max(maxArea, bfs(grid, i, j));
+		int n = grid[0].size();
+		int area = 0;
+		int offsets[] = {0, 1, 0, -1, 0}; // offsets are used to get surrounding coordinates
+        for (int i = 0; i < m; i++) { // iterate through each coordinate in the grid
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) { // only check the existing islands (if the coordinate value is 1)
+                    int a = 1; // area of the current island (starting at 1 to include the first coordinate)
+                    grid[i][j] = 0; // change to 0 to keep track of coordinates we've already checked
+                    queue<pair<int, int>> todo; // create queue of pairs to hold the coordinates 
+                    todo.push({i, j}); // add the current coordinates to the queue
+                    while (!todo.empty()) {
+                        pair<int, int> p = todo.front(); // create a pair containing the x, y coordinates of the current
+                        todo.pop(); // remove from the queue so we don't check it again
+                        for (int k = 0; k < 4; k++) { // check all 4 directions around the curr coordinate
+                            int r = p.first + offsets[k]; // change the x coordinate by the offset
+							int c = p.second + offsets[k + 1]; // change the y coordinate by the offset
+                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c]) { // check if it is valid
+                                grid[r][c] = 0; // change to 0 to tell that it's already been checked
+                                a++; // increase area by 1
+                                todo.push({r, c}); // push new offset coordinates into the queue to check its surrounding coordinates
+                            }
+                        }
+                    }
+                    area = max(area, a); // set the area to the max between the current max area and the new area just calculated
                 }
             }
         }
-        return maxArea;
-    }
-private:
-    int bfs(vector<vector<int>>& grid, int i, int j) {
-        queue<pair<int, int>> q;
-        q.push(pair<int, int>(i, j));
-        int maxArea = 1;
-        grid[i][j] = 0; // mark the value to avoid duplication
-        vector<int> dir({-1, 0, 1, 0, -1});
-        while (0 < q.size()) {
-            int m = q.front().first; // row index
-            int n = q.front().second; // col index
-            q.pop(); // remove from queue
-            for (int idx = 0; idx < dir.size() - 1; idx++) {
-                int rowIdx = m + dir[idx];
-                int colIdx = n + dir[idx + 1];
-                if (0<=rowIdx && rowIdx<grid.size() && 0<=colIdx && colIdx<grid[0].size()
-                    && 1==grid[rowIdx][colIdx]) {
-                    q.push(pair<int, int>(rowIdx, colIdx));
-                    maxArea++;
-                    grid[rowIdx][colIdx] = 0;
-                }
-            }
-        }
-        return maxArea;
+        return area;
     }
 };
